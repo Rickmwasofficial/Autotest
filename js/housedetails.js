e80bb2d08b7b41d042f282f0c5f793203ce2c8d3
Rickmwasofficial <146796160+Rickmwasofficial@users.noreply.github.com>
2024-04-20 20:11:00 +0300

    Update housedetails.js

--- js/housedetails.js
+++ js/housedetails.js
@@ -7,6 +7,7 @@ let availableHouses = [
 
 document.title = housedetails[0]
 document.addEventListener("DOMContentLoaded", function() {
+    
     imgs.innerHTML = `
     <div class="col col-12 mt-1 col-md-6">
     <img src="${availableHouses[0]}" alt="" width="auto" height="54" class="img-fluid">
@@ -32,25 +33,81 @@ document.addEventListener("DOMContentLoaded", function() {
     `
     moreActions.innerHTML = `
         <div class="textdet col-md-6 col-12">
-                    <h1>${housedetails[0]}</h1>
-                    <b>County: ${housedetails[4]}, Kenya</b><br>
-                    <b>Location: ${housedetails[5]}</b><br>
-                    <b>Room Type: ${housedetails[2]}</b><br>
-                    <b>${housedetails[6]}</b><br>
-                    <div class="review">
-                        <h2>Reviews</h2>
-                        <div class="reviewer">
-                            <img src="me.jpg">
-                            <div class="reviwerdet">
-                                <b style="padding-top: 4px;">John Doe</b><br>
-                                <b>Tenant</b><br>
-                                <b>Rating: 4.5/5.0</b>
+                    <h3>${housedetails[0]}</h3>
+                    <b><span class="text-danger">County: </span>${housedetails[4]}, Kenya</b><br>
+                    <b><span class="text-danger">Location: </span>${housedetails[5]}</b><br>
+                    <b><span class="text-danger">Room Type: </span>${housedetails[2]}</b><br>
+                    <b><span class="text-danger">${housedetails[6]}</span></b><br>
+                    <h5 class="mt-2">Reviews</h5>
+                    <div id="reviewsCarousel" class="carousel carousel-dark slidereview my-3 my-md-1" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#reviewsCarousel" data-bs-slide-to="0" class="active bg-transparent" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#reviewsCarousel" class="bg-transparent" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#reviewsCarousel" class="bg-transparent"  data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            <button type="button" data-bs-target="#reviewsCarousel" class="bg-transparent"  data-bs-slide-to="3" aria-label="Slide 4"></button>
+                        </div>
+                        <div class="carousel-inner container-fluid text-dark rounded border" style="width: 90%; height: 180px; overflow: hidden; overflow-y: scroll;">
+                            <div class="carousel-item active">
+                                <div class="reviewer">
+                                    <img src="me.jpg">
+                                    <div class="reviwerdet">
+                                        <b style="padding-top: 2px; font-size: 14px;">John Doe</b><br>
+                                        <b style="font-size: 14px;">Tenant</b><br>
+                                        <b style="font-size: 14px;">Rating: 4.5/5.0</b>
+                                    </div>
+                                </div>
+                                <div class="reviewtext">
+                                    <p>Despite its compact size, the room is efficiently designed to maximize functionality and comfort. The layout is well-planned, with designated areas for sleeping, studying, and relaxing. The bed is cozy and equipped with storage drawers underneath, perfect for keeping belongings organized in a limited space. The desk provides ample workspace for studying, with enough room for a laptop and textbooks.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="reviewer">
+                                    <img src="me.jpg">
+                                    <div class="reviwerdet">
+                                        <b style="padding-top: 2px; font-size: 14px;">Jane Doe</b><br>
+                                        <b style="font-size: 14px;">Tenant</b><br>
+                                        <b style="font-size: 14px;">Rating: 4.5/5.0</b>
+                                    </div>
+                                </div>
+                                <div class="reviewtext">
+                                    <p>Review2 Despite its compact size, the room is efficiently designed to maximize functionality and comfort. The layout is well-planned, with designated areas for sleeping, studying, and relaxing. The bed is cozy and equipped with storage drawers underneath, perfect for keeping belongings organized in a limited space. The desk provides ample workspace for studying, with enough room for a laptop and textbooks.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="reviewer">
+                                    <img src="me.jpg">
+                                    <div class="reviwerdet">
+                                        <b style="padding-top: 2px; font-size: 14px;">Mike Doe</b><br>
+                                        <b style="font-size: 14px;">Tenant</b><br>
+                                        <b style="font-size: 14px;">Rating: 4.5/5.0</b>
+                                    </div>
+                                </div>
+                                <div class="reviewtext">
+                                    <p>Review3 Despite its compact size, the room is efficiently designed to maximize functionality and comfort. The layout is well-planned, with designated areas for sleeping, studying, and relaxing. The bed is cozy and equipped with storage drawers underneath, perfect for keeping belongings organized in a limited space. The desk provides ample workspace for studying, with enough room for a laptop and textbooks.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="reviewer">
+                                    <img src="me.jpg">
+                                    <div class="reviwerdet">
+                                        <b style="padding-top: 2px; font-size: 14px;">Josephine Doe</b><br>
+                                        <b style="font-size: 14px;">Tenant</b><br>
+                                        <b style="font-size: 14px;">Rating: 4.5/5.0</b>
+                                    </div>
+                                </div>
+                                <div class="reviewtext">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit explicabo suscipit eos exercitationem, minus nemo libero fuga nostrum accusantium odio sed laudantium natus quis laborum magnam quo vitae porro ea?</p>
+                                </div>
                             </div>
                         </div>
-                        <div class="reviewtext">
-                            
-                            <p>Despite its compact size, the room is efficiently designed to maximize functionality and comfort. The layout is well-planned, with designated areas for sleeping, studying, and relaxing. The bed is cozy and equipped with storage drawers underneath, perfect for keeping belongings organized in a limited space. The desk provides ample workspace for studying, with enough room for a laptop and textbooks.</p>
-                        </div>
+                        <button class="carousel-control-prev me-4" type="button" data-bs-target="#reviewsCarousel" data-bs-slide="prev" style="width: 5%;">
+                            <span class="carousel-control-prev-icon bg-transparent pe-1" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next ms-4" type="button" data-bs-target="#reviewsCarousel" data-bs-slide="next" style="width: 5%;">
+                            <span class="carousel-control-next-icon bg-transparent text-danger" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     </div>
                 </div>
                 <div class="book col-md-6 col-12">
@@ -70,9 +127,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         <button class="report container">
                             <b>Report House</b>
                         </button>
-                        
                     </div>
-    
                 </div>
     `
 })
